2ab416fbcd90a100c0a269e741975041312e4730
Matias <matias.infra@gmail.com>
2024-02-23 11:59:25 -0300

    agregando mensaje de error al name y lastname

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function validateForm () {
   const password = $("#input-password").val();
   const bike = $("#bike").val();
   const social = $("#input-social").val();
+  const mensajesError = $(".mensajesError") .val();
 
   let tipoBicicleta = $('select[name=bike]').find("option:selected").val();
 
@@ -14,19 +15,26 @@ function validateForm () {
   const validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
 
   if(name.length < 1 || lastname.length < 1 || email.length < 1 || password.length < 1 || social < 1){
-    alert('Debe rellenar todos los campos')
+    mensajeDeError("name","debe llenar todos los campos")
+    mensajeDeError("lastname","debe llenar todos los campos")
+    mensajeDeError("input-email","debe llenar todos los campos")
+    mensajeDeError("input-password","debe llenar todos los campos")
   }
-  if(name.charAt(0) != name.charAt(0).toUpperCase() || lastname.charAt(0) != lastname.charAt(0).toUpperCase()){
+  if(name.charAt(0) != name.charAt(0).toUpperCase()){
 
-    alert('la primera letra de los campos nombre y apellido debe ser en mayusculas')
+    mensajeDeError("name","La primera letra debe estar en mayúsculas")
 
 
+  }
+  if(lastname.charAt(0) != lastname.charAt(0).toUpperCase()){
+    mensajeDeError("lastname","La primera letra debe estar en mayúsculas")
   }
   if(name.length < 3 || name.length > 8){
-    alert('El campo nombre debe tener entre 3 y 8 caracteres')
+    mensajeDeError("name","El campo nombre debe tener entre 3 y 8 caracteres")
+
   }
   if(lastname.length < 3 || lastname.length > 8){
-    alert('El campo apellido debe tener entre 3 y 8 caracteres')
+    mensajeDeError("lastname","El campo apellido debe tener entre 3 y 8 caracteres")
   }
 
   if(!expresionRegular.test(password)){
@@ -38,6 +46,10 @@ function validateForm () {
   }
   if(tipoBicicleta == 0 ){
     alert('debe seleccionar un tipo de bicicleta')
+  }
+
+  function mensajeDeError(variable,mensaje){
+    return $("#"+`${variable}`).parent().append("<span>"+mensaje+"</span>");
   }
 
 
